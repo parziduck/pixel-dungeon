f9393ef45370e30f577ec935cda53ad8ca097adb
parziduck <39910706+parziduck@users.noreply.github.com>
2018-06-04 21:13:58 +0800

    Update Tempering.java

--- src/com/watabou/pixeldungeon/items/weapon/enchantments/Tempering.java
+++ src/com/watabou/pixeldungeon/items/weapon/enchantments/Tempering.java
@@ -18,30 +18,51 @@
 package com.watabou.pixeldungeon.items.weapon.enchantments;
 
 import com.watabou.pixeldungeon.actors.Char;
+import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.items.weapon.Weapon;
+import com.watabou.pixeldungeon.sprites.CharSprite;
 import com.watabou.pixeldungeon.sprites.ItemSprite;
 import com.watabou.pixeldungeon.sprites.ItemSprite.Glowing;
+import com.watabou.utils.Random;
 
-public class Tempering extends Weapon.Enchantment {
+public class Leech extends Weapon.Enchantment {
 
-	private static final String TXT_TEMPERED	= "tempered %s";
+	private static final String TXT_VAMPIRIC	= "vampiric %s";
 	
-	private static ItemSprite.Glowing GRAY = new ItemSprite.Glowing( 0xCC8888 );
+	private static ItemSprite.Glowing RED = new ItemSprite.Glowing( 0x660022 );
 	
 	@Override
 	public boolean proc( Weapon weapon, Char attacker, Char defender, int damage ) {
-		weapon.polish();
-		return true;
+		
+		int level = Math.max( 0, weapon.effectiveLevel() );
+		
+		// lvl 0 - 33%
+		// lvl 1 - 43%
+		// lvl 2 - 50%
+		int maxValue = damage * (level + 2) / (level + 6);
+		int effValue = Math.min( Random.IntRange( 0, maxValue ), attacker.HT - attacker.HP );
+		
+		if (effValue > 0) {
+		
+			attacker.HP += effValue;
+			attacker.sprite.emitter().start( Speck.factory( Speck.HEALING ), 0.4f, 1 );
+			attacker.sprite.showStatus( CharSprite.POSITIVE, Integer.toString( effValue ) );
+			
+			return true;
+			
+		} else {
+			return false;
+		}
 	}
 	
 	@Override
 	public Glowing glowing() {
-		return GRAY;
+		return RED;
 	}
 	
 	@Override
-	public String name( String weaponName) {
-		return String.format( TXT_TEMPERED, weaponName );
+	public String name( String weaponName ) {
+		return String.format( TXT_VAMPIRIC, weaponName );
 	}
 
 }
