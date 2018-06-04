45461460de6a70fe72a704e633c95ce9a0f3fd77
parziduck <39910706+parziduck@users.noreply.github.com>
2018-06-04 21:13:02 +0800

    Update Paralysis.java

--- src/com/watabou/pixeldungeon/items/weapon/enchantments/Paralysis.java
+++ src/com/watabou/pixeldungeon/items/weapon/enchantments/Paralysis.java
@@ -18,31 +18,38 @@
 package com.watabou.pixeldungeon.items.weapon.enchantments;
 
 import com.watabou.pixeldungeon.actors.Char;
-import com.watabou.pixeldungeon.actors.buffs.Buff;
+import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.items.weapon.Weapon;
+import com.watabou.pixeldungeon.sprites.CharSprite;
 import com.watabou.pixeldungeon.sprites.ItemSprite;
 import com.watabou.pixeldungeon.sprites.ItemSprite.Glowing;
 import com.watabou.utils.Random;
 
-public class Paralysis extends Weapon.Enchantment {
+public class Leech extends Weapon.Enchantment {
 
-	private static final String TXT_STUNNING = "stunning %s";
+	private static final String TXT_VAMPIRIC	= "vampiric %s";
 	
-	private static ItemSprite.Glowing YELLOW = new ItemSprite.Glowing( 0xCCAA44 );
+	private static ItemSprite.Glowing RED = new ItemSprite.Glowing( 0x660022 );
 	
 	@Override
 	public boolean proc( Weapon weapon, Char attacker, Char defender, int damage ) {
-		// lvl 0 - 13%
-		// lvl 1 - 22%
-		// lvl 2 - 30%
+		
 		int level = Math.max( 0, weapon.effectiveLevel() );
 		
-		if (Random.Int( level + 8 ) >= 7) {
-			
-			Buff.prolong( defender, com.watabou.pixeldungeon.actors.buffs.Paralysis.class, 
-				Random.Float( 1, 1.5f + level ) );
+		// lvl 0 - 33%
+		// lvl 1 - 43%
+		// lvl 2 - 50%
+		int maxValue = damage * (level + 2) / (level + 6);
+		int effValue = Math.min( Random.IntRange( 0, maxValue ), attacker.HT - attacker.HP );
+		
+		if (effValue > 0) {
+		
+			attacker.HP += effValue;
+			attacker.sprite.emitter().start( Speck.factory( Speck.HEALING ), 0.4f, 1 );
+			attacker.sprite.showStatus( CharSprite.POSITIVE, Integer.toString( effValue ) );
 			
 			return true;
+			
 		} else {
 			return false;
 		}
@@ -50,12 +57,12 @@ public class Paralysis extends Weapon.Enchantment {
 	
 	@Override
 	public Glowing glowing() {
-		return YELLOW;
+		return RED;
 	}
 	
 	@Override
-	public String name( String weaponName) {
-		return String.format( TXT_STUNNING, weaponName );
+	public String name( String weaponName ) {
+		return String.format( TXT_VAMPIRIC, weaponName );
 	}
 
 }
