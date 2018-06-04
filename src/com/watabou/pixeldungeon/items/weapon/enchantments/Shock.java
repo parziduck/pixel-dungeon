44a6d15832ced69f9e41902276a40cf0de041bf1
parziduck <39910706+parziduck@users.noreply.github.com>
2018-06-04 21:13:31 +0800

    Update Shock.java

--- src/com/watabou/pixeldungeon/items/weapon/enchantments/Shock.java
+++ src/com/watabou/pixeldungeon/items/weapon/enchantments/Shock.java
@@ -17,84 +17,52 @@
  */
 package com.watabou.pixeldungeon.items.weapon.enchantments;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-
-import com.watabou.pixeldungeon.actors.Actor;
 import com.watabou.pixeldungeon.actors.Char;
-import com.watabou.pixeldungeon.effects.Lightning;
-import com.watabou.pixeldungeon.effects.particles.SparkParticle;
+import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.items.weapon.Weapon;
-import com.watabou.pixeldungeon.levels.Level;
-import com.watabou.pixeldungeon.levels.traps.LightningTrap;
+import com.watabou.pixeldungeon.sprites.CharSprite;
+import com.watabou.pixeldungeon.sprites.ItemSprite;
+import com.watabou.pixeldungeon.sprites.ItemSprite.Glowing;
 import com.watabou.utils.Random;
 
-public class Shock extends Weapon.Enchantment {
+public class Leech extends Weapon.Enchantment {
 
-	private static final String TXT_SHOCKING	= "shocking %s";
+	private static final String TXT_VAMPIRIC	= "vampiric %s";
+	
+	private static ItemSprite.Glowing RED = new ItemSprite.Glowing( 0x660022 );
 	
 	@Override
 	public boolean proc( Weapon weapon, Char attacker, Char defender, int damage ) {
-		// lvl 0 - 25%
-		// lvl 1 - 40%
-		// lvl 2 - 50%
+		
 		int level = Math.max( 0, weapon.effectiveLevel() );
 		
-		if (Random.Int( level + 4 ) >= 3) {
-			
-			points[0] = attacker.pos;
-			nPoints = 1;
-			
-			affected.clear();
-			affected.add( attacker );
-			
-			hit( defender, Random.Int( 1, damage / 2 ) );
-			
-			attacker.sprite.parent.add( new Lightning( points, nPoints, null ) );
+		// lvl 0 - 33%
+		// lvl 1 - 43%
+		// lvl 2 - 50%
+		int maxValue = damage * (level + 2) / (level + 6);
+		int effValue = Math.min( Random.IntRange( 0, maxValue ), attacker.HT - attacker.HP );
+		
+		if (effValue > 0) {
+		
+			attacker.HP += effValue;
+			attacker.sprite.emitter().start( Speck.factory( Speck.HEALING ), 0.4f, 1 );
+			attacker.sprite.showStatus( CharSprite.POSITIVE, Integer.toString( effValue ) );
 			
 			return true;
 			
 		} else {
-			
 			return false;
-			
 		}
 	}
 	
 	@Override
+	public Glowing glowing() {
+		return RED;
+	}
+	
+	@Override
 	public String name( String weaponName ) {
-		return String.format( TXT_SHOCKING, weaponName );
+		return String.format( TXT_VAMPIRIC, weaponName );
 	}
 
-	private ArrayList<Char> affected = new ArrayList<Char>();
-	
-	private int[] points = new int[20];
-	private int nPoints;
-	
-	private void hit( Char ch, int damage ) {
-		
-		if (damage < 1) {
-			return;
-		}
-		
-		affected.add( ch );
-		ch.damage( Level.water[ch.pos] && !ch.flying ? (int)(damage * 2) : damage, LightningTrap.LIGHTNING  );
-		
-		ch.sprite.centerEmitter().burst( SparkParticle.FACTORY, 3 );
-		ch.sprite.flash();
-		
-		points[nPoints++] = ch.pos;
-		
-		HashSet<Char> ns = new HashSet<Char>();
-		for (int i=0; i < Level.NEIGHBOURS8.length; i++) {
-			Char n = Actor.findChar( ch.pos + Level.NEIGHBOURS8[i] );
-			if (n != null && !affected.contains( n )) {
-				ns.add( n );
-			}
-		}
-		
-		if (ns.size() > 0) {
-			hit( Random.element( ns ), Random.Int( damage / 2, damage ) );
-		}
-	}
 }
